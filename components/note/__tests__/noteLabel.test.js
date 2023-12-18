b1dcc6c447eda2f7642842c5a17f95a7b16d78a3
MuskanFatimaa <120410416+MuskanFatimaa@users.noreply.github.com>
2023-12-19 01:20:47 +0500

    Delete components directory

--- components/note/__tests__/noteLabel.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import NoteLabel from "../NoteLabel.js";
-
-test("Shows label prop correctly", () => {
-  render(<NoteLabel label="new label" />);
-  const labelContainer = screen.getByText("new label");
-  expect(labelContainer).toBeInTheDocument();
-});
